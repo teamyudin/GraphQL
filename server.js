e2ff932227c6c5ef9d1472f2e826c304c1daaf72
Void Ray <team.yudin@gmail.com>
2017-07-06 14:54:07 -0500

    Added express-vue

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
 import bodyParser from 'body-parser';
 import schema from './data/schema';
 
+import path from 'path';
+import expressVue from 'express-vue';
+
 const GRAPHQL_PORT = 3000;
 
 const graphQLServer = express();
@@ -13,3 +16,24 @@ graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 graphQLServer.listen(GRAPHQL_PORT, () => console.log(
   `GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphiql`
 ));
+
+const app = express();
+
+app.engine('vue', expressVue);
+app.set('view engine', 'vue');
+app.set('views', path.join(__dirname, '/views'));
+app.set('vue', {
+    componentsDir: path.join(__dirname, '/views/components')
+});
+
+app.get('/', function(req, res){
+  var scope = {
+    vue: {
+      components: ['message'],
+    }
+  };
+    res.render('index', scope);
+});
+
+app.listen(3001);
+console.log('Express server listening on port 3000');
---
+++ views/index.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+    <div>
+      <message :message="message"></message>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+          message: "Hello World"
+        }
+    }
+}
+</script>
+
+<style lang="css">
+</style>
