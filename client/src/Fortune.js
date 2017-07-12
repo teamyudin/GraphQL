8d94bfcea7cb6ec77c1a12c83da54b2e2cf9beab
Void Ray <team.yudin@gmail.com>
2017-07-12 13:41:37 -0500

    fortune is now rendering

--- client/src/Fortune.js
+++ client/src/Fortune.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { gql, graphql} from 'react-apollo';
 
-export const Fortune = ({ data: { loading, fortune, error } }) => {
-  if (loading) return <div>Loading</div>;
+export const Fortune = ({ data: { loading, getFortuneCookie, error } }) => {
+  if (loading) return <div>Loading...</div>;
   if (error) return <h1>ERROR</h1>;
   return (
     <div>
     Fortune:
       {
-        fortune
+        getFortuneCookie
       }
     </div>
   )
